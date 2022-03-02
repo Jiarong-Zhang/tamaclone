0332c07b95521334978dbb2b2bfa4f58109f8bc7
Emily Zhang <emilyzhang@Emilys-Macbook.local>
2022-03-02 15:21:32 +1030

    facing

--- source/main.lua
+++ source/main.lua
@@ -21,7 +21,7 @@ function setup()
 	sprite:add()
 
 	gfx.setColor(gfx.kColorBlack)
-	gfx.setLineWidth(3)
+	gfx.setLineWidth(3) 
 end
 
 setup()
@@ -31,8 +31,6 @@ function drawSprite( )
 
 	gfx.drawRoundRect(32, 32, 336, 176, 5)		-- border
 
-
-
 end
 
 
@@ -41,27 +39,38 @@ end
 
 -- randomly select a movement direction
 -- and randomly choose the movement magnitude
+local faceL = true
 function shuffle( )
 
 	local rDirection = math.random(0,4)
 	local rMagnitude = math.random(1,10)
 	--print(rDirection)
 
-	if rDirection  == 0 then
+	if rDirection  == 0 then	-- move left
 		sprite.x = sprite.x - rMagnitude
+		if faceL == false then
+			sprite:setImageFlip(gfx.kImageUnflipped)	-- reset facing
+			faceL = true
+		end
 	end
-	if rDirection == 1 then
+	if rDirection == 1 then		-- move right
 		sprite.x = sprite.x + rMagnitude
+		if faceL == true then
+			sprite:setImageFlip(gfx.kImageFlippedX)	-- flip facing
+			faceL = false
+		end
 	end
-	if rDirection == 2 then
+	if rDirection == 2 then		-- move down
 		sprite.y = sprite.y - rMagnitude
 	end
-	if rDirection == 3 then
+	if rDirection == 3 then		-- move up
 		sprite.y = sprite.y + rMagnitude
 	end
 	if rDirection == 4 then
 		return
 	end
+	--print(faceL)
+
 end
 
 -- only update sprite location if one seconds has passed
@@ -71,13 +80,11 @@ function movement( )
 
 	local elapsed = playdate.getCurrentTimeMilliseconds()
 
-	if (elapsed - timeSince) < 750 then
-		return
+	if (elapsed - timeSince) > 750 then
+		shuffle()
+		sprite:moveTo(sprite.x, sprite.y)	-- move sprite
+		timeSince = elapsed		-- update time of last movement event
 	end
-
-	shuffle()
-	sprite:moveTo(sprite.x, sprite.y)	-- move sprite
-	timeSince = elapsed		-- update time of last movement event
 end
 
 
@@ -85,7 +92,7 @@ end
 function playdate.update( )
 
 	gfx.sprite.update();
-	playdate.timer.updateTimers();
+	playdate.timer.updateTimers()
 	drawSprite()
-	movement();
+	movement()
 end
